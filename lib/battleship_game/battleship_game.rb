120b2d1986188beae045701cadd05615537912cd
dave woodall <github@davewoodall.com>
2019-02-27 17:38:15 -0500

    Milestone 1: Create Grid
    Create 10x10 grid as well as provide getter and setter methods for interacting with
    it.

--- lib/battleship_game/battleship_game.rb
+++ lib/battleship_game/battleship_game.rb
@@ -1,6 +1,49 @@
 module BattleshipGame
   class BattleshipGame
+
+    attr_reader :grid, :columns
+
+    def initialize
+      @grid = create_grid
+      @columns = make_columns
+    end
+
     def fire!(position)
+    end
+
+# private
+
+    def get(pos)
+      row = get_row pos[1]
+      col = get_column pos[0]
+      row[col]
+    end
+
+    def set(pos, item)
+      row = get_row pos[1]
+      col = get_column pos[0]
+      row[col] = item
+    end
+
+    def get_column(col)
+      @columns.find { |c| c[0] === col }[1]
+    end
+
+    def make_columns
+      ('A'..'J').map.with_index do |let, i|
+        [let, i]
+      end
+    end
+
+    def get_row(row)
+      r = row.to_i - 1
+      grid[r]
+    end
+
+    def create_grid
+      Array.new(10) do
+        Array.new(10)
+      end
     end
   end
 end
--- spec/milestone_spec.rb
+++ spec/milestone_spec.rb
@@ -1,8 +1,37 @@
 RSpec.describe BattleshipGame::BattleshipGame do
-  describe 'Milestones' do
-    it 'place ship on grid'
-    it 'place ships on board'
-    it 'implement fire method'
-    it 'sinks a ship'
+  fdescribe 'Milestones' do
+    subject { BattleshipGame::BattleshipGame.new }
+
+    context 'create a 10 x 10 grid' do
+      it "is created on initialization" do
+        result = subject.grid
+        expect = Array.new(10) { Array.new(10) }
+        expect(result).to eq(expect)
+      end
+
+      it 'map column letter to position' do
+        result = subject.columns.first
+        expect = ["A", 0]
+        expect(result).to eq(expect)
+      end
+
+      it 'get grid position' do
+        subject.grid[0][0] = 'boom'
+        result = subject.get('A1')
+        expect = 'boom'
+        expect(result).to eq(expect)
+      end
+
+      it 'set grid with content' do
+        subject.set('A1', 'pow')
+        result = subject.get('A1')
+        expect = 'pow'
+        expect(result).to eq(expect)
+      end
+    end
+
+    context 'place ships on board'
+    context 'implement fire method'
+    context 'sinks a ship'
   end
 end
